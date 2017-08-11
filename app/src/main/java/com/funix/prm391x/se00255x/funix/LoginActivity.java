bf6b05d54fea43b3e98a09f562bf86f36d207d6f
Tai Bui <30861484+taibuiSW@users.noreply.github.com>
2017-08-11 18:27:30 +0700

    Initial commit

---
+++ app/src/main/java/com/funix/prm391x/se00255x/funix/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.funix.prm391x.se00255x.funix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
