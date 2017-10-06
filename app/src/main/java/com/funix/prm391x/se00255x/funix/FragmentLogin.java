8e60c78913cdda430b3303d2c0b8edf2ace8d98e
Tai Bui <30861484+taibuiSW@users.noreply.github.com>
2017-10-06 17:13:01 +0700

    firebase auth (log in/sign in)

--- app/src/main/java/com/funix/prm391x/se00255x/funix/FragmentLogin.java
+++ app/src/main/java/com/funix/prm391x/se00255x/funix/FragmentLogin.java
@@ -1,30 +1,58 @@
 package com.funix.prm391x.se00255x.funix;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FragmentLogin extends Fragment {
     EditText mEdtEmail;
     EditText mEdtPassword;
     Button mBtnLogin;
+    FirebaseAuth mAuth;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         mEdtEmail = (EditText) view.findViewById(R.id.edt_email);
-        Log.e("___email", "" + (mEdtEmail ==null));
         mEdtPassword = (EditText) view.findViewById(R.id.edt_password);
-        Log.e("___password", "" + (mEdtPassword ==null));
         mBtnLogin = (Button) view.findViewById(R.id.btn_log_in);
-        Log.e("___btn", "" + (mBtnLogin ==null));
+        mAuth = FirebaseAuth.getInstance();
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email    = mEdtEmail.getText().toString().trim();
+                String password = mEdtPassword.getText().toString().trim();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message = "Log in successfully!";
+                                if (!task.isSuccessful()) {
+                                    try {
+                                        throw task.getException();
+                                    } catch (Exception e) {
+                                        message = e.getMessage();
+                                    }
+                                }
+                                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
         return view;
     }
 }
+
